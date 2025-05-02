8d27b9e4a4df7c3782b9862b308f0bbdead030e3
vidhishaprabhu <vidhishaprabhu@gmail.com>
2025-05-02 19:01:30 +0530

    Added Added Welcome message on the Support.vue page got logged in user

--- crm-frontend/src/components/SupportDashboard.vue
+++ crm-frontend/src/components/SupportDashboard.vue
@@ -1,11 +1,43 @@
 <template>
 <div>
-  <h2>Welcome to Support</h2>
+  <p class="text-center mb-6 text-primary" v-if="loggedInUser">
+      Hello {{ loggedInUser.name }}
+    </p>
+    <h2 class="text-center mb-6 text-primary" v-if="loggedInUser">Welcome to {{loggedInUser.name}}!</h2>
+    <h2>Getting Started</h2>
+    <br>
 </div>
   
 </template>
 <script>
+import api from '../api'; 
 export default{
+  data(){
+    return{
+      id:'',
+      name:'',
+      email:'',
+      loggedInUser: null, 
+    }
+  },
+  mounted() {
+    this.fetchLoggedInUser();
+  },
+  methods:{
+    async fetchLoggedInUser() {
+      try {
+        const token = localStorage.getItem('api-token');
+        const response = await api.get('/user', {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        });
+        this.loggedInUser = response.data;
+      } catch (error) {
+        this.error = "Failed to fetch logged-in user";
+      }
+    },
+  }
   
 }
 </script>
