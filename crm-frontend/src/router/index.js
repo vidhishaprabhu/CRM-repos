044dbf96371b6595f161cdf5451a8908b5a07fa8
vidhishaprabhu <vidhishaprabhu@gmail.com>
2025-05-04 11:59:19 +0530

    Updated the index.js in-order to add dropdown under dashboard

--- crm-frontend/src/router/index.js
+++ crm-frontend/src/router/index.js
@@ -4,13 +4,18 @@ import Login from '../components/Login.vue';
 import AdminDashboard from '../components/AdminDashboard.vue';
 import SalesDashboard from '../components/SalesDashboard.vue';
 import SupportDashboard from '../components/SupportDashboard.vue';
+import Dashboard from '../components/Dashboard.vue';
+import LoginChart from '../components/LoginChart.vue';
 
 const routes = [
   { path: '/register', component: Register },
   { path: '/login', component: Login },
-  { path: '/admin-dashboard', component: AdminDashboard, meta: { requiresAuth: true, role: 'Admin' } },
-  { path: '/sales-dashboard', component: SalesDashboard, meta: { requiresAuth: true, role: 'Sales Manager' } },
-  { path: '/support-dashboard', component: SupportDashboard, meta: { requiresAuth: true, role: 'Support' } }
+  { path: '/dashboard', component: Dashboard, meta: { requiresAuth: true }, children: [
+    { path: '/admin', component: AdminDashboard, meta: { requiresAuth: true, role: 'Admin' } },
+    { path: '/sales', component: SalesDashboard, meta: { requiresAuth: true, role: 'Sales Manager' } },
+    { path: '/support', component: SupportDashboard, meta: { requiresAuth: true, role: 'Support' } },
+  ] },
+  { path: '/login-chart', component: LoginChart, meta: { requiresAuth: true } },
 ];
 
 const router = createRouter({
@@ -20,21 +25,21 @@ const router = createRouter({
 
 router.beforeEach((to, from, next) => {
   const token = localStorage.getItem('api-token');
-  const userRole = localStorage.getItem('user-role'); 
+  const userRole = localStorage.getItem('user-role');
 
   if (to.meta.requiresAuth) {
     if (!token) {
-      next('/login'); 
+      next('/login');
     } else {
       if (to.meta.role && to.meta.role !== userRole) {
         alert('Access Denied: Unauthorized Role');
-        next('/login'); 
+        next('/login');
       } else {
-        next(); 
+        next();
       }
     }
   } else {
-    next(); 
+    next();
   }
 });
 
