e91ce26ae7cd5a7c673bfc5d837fb783c62bde23
vidhishaprabhu <vidhishaprabhu@gmail.com>
2025-05-11 14:55:55 +0530

    Created the migration file to create the Pipeline table

---
+++ crm-backend/database/migrations/2025_05_11_083609_create_pipelines_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePipelinesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pipelines', function (Blueprint $table) {
+            $table->id();
+            $table->string('deal_title');
+            $table->string('customer_name');
+            $table->decimal('deal_value',10,2);
+            $table->date('expected_close_date');
+            $table->string('sales_stage');
+            $table->string('assigned_to');
+            $table->text('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pipelines');
+    }
+}
