ecafc6e342b73e3c331bcbe8d6f0dbe19a8c1642
vidhishaprabhu <vidhishaprabhu@gmail.com>
2025-05-25 19:08:33 +0530

    Added the controller for lead and the function to save the information related to adding the lead

--- crm-backend/app/Http/Controllers/LeadController.php
+++ crm-backend/app/Http/Controllers/LeadController.php
@@ -8,18 +8,43 @@ use Illuminate\Http\Request;
 class LeadController extends Controller
 {
     public function store(Request $request){
-        $validated=$request->validated([
+        $validated=$request->validate([
             'full_name'=>'required|string|max:255',
             'email'=>'required|email|max:255',
-            'phone'=>'required|email|max:10',
-            'company'=>'required|str',
+            'phone'=>'required|digits:10',
+            'company'=>'required|string',
             'job_title'=>'required|string|max:255',
             'lead_source'=>'required|in:Website,Referral,Social,Media,Event,Other',
-            'status'=>'required|in:New,COntacted,Qualified,Lost',
+            'status'=>'required|in:New,Contacted,Qualified,Lost',
             'assigned_to'=>'required|string|max:255',
             'notes'=>'nullable|string',
-        ]);
-        $lead = Lead::create($validated);
-        return response()->json($lead,201);
-    }
+        ],[
+        'full_name.required' => 'Please enter the full name.',
+        'email.required'=>'Please enter the email',
+        'phone'=>'Please enter the phone number',
+        'phone.max'=>'Phone number should be maximum 10 digits',
+        'company'=>'Please enter the name of the company',
+        'job-title'=>'Please enter the job-title',
+        'lead-source'=>'Please enter the lead-source',
+        'status'=>'Please enter the status',
+        'assigned-to'=>'Please eneter the assigned-to field',
+        'notes'=>'Please enter the notes'    
+    ]);
+        $lead = new Lead();
+        $lead->full_name = $request->full_name;
+        $lead->email = $request->email;
+        $lead->phone = $request->phone;
+        $lead->company = $request->company;
+        $lead->job_title = $request->job_title;
+        $lead->lead_source = $request->lead_source;
+        $lead->status = $request->status;
+        $lead->assigned_to = $request->assigned_to;
+        $lead->notes = $request->notes;
+        $lead->save();
+
+    return response()->json([
+        'message' => 'Lead entry created successfully',
+        'data' => $lead
+    ], 201);
+}
 }
