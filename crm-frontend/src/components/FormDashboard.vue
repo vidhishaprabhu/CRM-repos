720f51a1aa998ab5546f11470dcbd2c5d9925903
vidhishaprabhu <vidhishaprabhu@gmail.com>
2025-06-01 12:37:16 +0530

    Added the breadcrumbs and button for create form and ant design

--- crm-frontend/src/components/FormDashboard.vue
+++ crm-frontend/src/components/FormDashboard.vue
@@ -1,11 +1,29 @@
 <template>
 <div>
-  <h5>Welcome to Form-Dashboard</h5>
+  <nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item"><router-link to="/main-dashboard">Dashboard</router-link></li>
+      <li class="breadcrumb-item"><router-link to="/sales">Lead Management</router-link></li>
+      <li class="breadcrumb-item"><router-link to="/sales">All Forms</router-link></li>
+    </ol>
+  </nav>
+
+  <h4>All Forms</h4>
+
+  <div class="d-flex justify-content-end">
+    <Button className="btn btn-primary" style="font-weight:700"><PlusOutlined /> Create Form</Button>
+  </div>
 </div>
+
   
 </template>
 <script>
+import { Button,PlusOutlined } from '@ant-design/icons-vue';
 export default{
   name:'FormDashboard',
+  components:{
+    Button,
+    PlusOutlined
+  }
 }
 </script>
