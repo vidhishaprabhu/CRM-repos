50283eb6e66890b1f14dc02a6b2541902cf0a3b0
vidhishaprabhu <vidhishaprabhu@gmail.com>
2025-06-16 19:03:50 +0530

    Created the migration for creating table for form

---
+++ crm-backend/database/migrations/2025_06_16_105506_create_forms_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFormsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('forms', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('name')->nullable();
+            $table->text('submit_message')->nullable();
+            $table->string('logo')->nullable(); 
+            $table->json('fields');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('forms');
+    }
+}
